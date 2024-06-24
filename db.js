d88af083716e9d4751c97c26a563a4dc8c305cce
Matt Saemrow <mattsaemrow@gmail.com>
2024-06-24 15:30:59 -0500

    updated db.js file again to connect to db correctly

--- db.js
+++ db.js
@@ -4,14 +4,15 @@ const { Client } = require("pg");
 const { getDatabaseName } = require("./config");
 require('dotenv').config();
 
-
-const db = new Client({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DATABASE_URL || getDatabaseName()
-});
+const dbConfig = process.env.DATABASE_URL ? 
+  { connectionString: process.env.DATABASE_URL } : 
+  {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: getDatabaseName()
+  };
 
 console.log("Database connection details:");
 console.log("DB_USER:", process.env.DB_USER);
@@ -19,6 +20,8 @@ console.log("DB_PASS:", process.env.DB_PASS);
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_PORT:", process.env.DB_PORT);
 console.log("Database Name:", getDatabaseName());
+
+const db = new Client(dbConfig);
 
 db.connect();
 
