b6295a04a6799fa1d9d2e5158b9b91a0a65f0ca4
Matt Saemrow <mattsaemrow@gmail.com>
2024-06-24 15:36:31 -0500

    reverting back to previous code as that deployed correctly

--- db.js
+++ db.js
@@ -4,21 +4,15 @@ const { Client } = require("pg");
 const { getDatabaseName } = require("./config");
 require('dotenv').config();
 
-
-let db;
-
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
-}
+const dbConfig = process.env.DATABASE_URL ? 
+  { connectionString: process.env.DATABASE_URL } : 
+  {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: getDatabaseName()
+  };
 
 console.log("Database connection details:");
 console.log("DB_USER:", process.env.DB_USER);
@@ -26,6 +20,8 @@ console.log("DB_PASS:", process.env.DB_PASS);
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_PORT:", process.env.DB_PORT);
 console.log("Database Name:", getDatabaseName());
+
+const db = new Client(dbConfig);
 
 db.connect();
 
